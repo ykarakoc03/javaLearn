664ca8a2a34823a05f809dcec71b8aea1e473948
ykarakoc03 <delisuvarim@gmail.com>
2022-10-01 23:04:31 +0300

    first commit

---
+++ src/j11_MethodCreation/tasks/Task06.java
@@ -0,0 +1,17 @@
+package j11_MethodCreation.tasks;
+
+import java.util.Scanner;
+
+public class Task06 {
+
+	public static void main(String[] args) {
+		/*
+		 Kullanıcıya paralelkenar, dikdörtgen ve üçgen kelimelerinden birini 
+		 ve iki sayı seçmesini söyleyin. 
+         Hangi şekli seçerse, o şeklin alanını ve çevresini ekrana yazdıran programı yazınız.
+		 */
+		
+
+	}
+
+}
